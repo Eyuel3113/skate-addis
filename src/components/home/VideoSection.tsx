3247fed3e685d2c936c35d9705a6dfaa507c8f60
smucav <smucav@gmail.com>
2025-11-28 07:58:04 -0800

    video section

--- src/components/home/VideoSection.tsx
+++ src/components/home/VideoSection.tsx
@@ -87,7 +87,7 @@ export function VideoSection() {
                                     <iframe
                                         width="100%"
                                         height="100%"
-                                        src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
+                                        src="/hero.png"
                                         title="YouTube video player"
                                         frameBorder="0"
                                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
