321a4db044a62776cf0ba40640d5628858972707
Eyuel_Endale <eyuelendale26@gmail.com>
2025-11-29 12:06:22 +0300

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,24 +1,3 @@
-// "use client";
-
-// import { Story } from "@/components/about/Story";
-// import { Team } from "@/components/about/Team";
-// import { Timeline } from "@/components/about/Timeline";
-// import { PageHeader } from "@/components/ui/PageHeader";
-
-// export default function AboutPage() {
-//     return (
-//         <>
-//             <PageHeader
-//                 title="ABOUT US"
-//                 description="Building a community through the power of skateboarding."
-//                 backgroundImage="https://images.unsplash.com/photo-1564982752979-3f7bc974d29a?q=80&w=2070&auto=format&fit=crop"
-//             />
-//             <Story />
-//             <Timeline />
-//             {/* <Team /> */}
-//         </>
-//     );
-// }
 "use client";
 import { Story } from "@/components/about/Story";
 import { Team } from "@/components/about/Team";
